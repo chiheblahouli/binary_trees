0fd6ee34165c401d3767b4d545f959b1a54be83e
chiheblahouli <chiheb.lahouli@gmail.com>
2020-07-22 10:20:31 +0000

    c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - a function that creates a binary tree node.
+* @parent: pointer of the node to create.
+* @value: Value of the new node.
+* Return: Always 0 if failure.
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = new_node->left =NULL;
+	return (new_node);
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# binary_trees
+0x1D. C - Binary trees
+What is a binary tree
+What is the difference between a binary tree and a Binary Search Tree
+What are the depth, the height, the size of a binary tree
