6fefcd114da39edb0346c18320e893869132cd1f
chiheblahouli <chiheb.lahouli@gmail.com>
2020-07-24 23:01:18 +0000

    c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - compute the depth of BT recursively.
+ * @tree: pointer.
+ * Return: depth of the tree.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t x = 0;
+	if (tree == NULL)
+		return (0);
+	if tree = tree->parent
+{
+	x += 1;
+}
+	return (x);
+}
