c3c09d6cde88b234db38e4e44a947ac87e39b713
chiheblahouli <chiheb.lahouli@gmail.com>
2020-07-24 22:51:33 +0000

    c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,20 +1,21 @@
-#include <sdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
 * binary_tree_postorder - this is function
 * @tree: recive pointer.
 * @func: recive pointer.
-* section header: section descript
+* Return: Always 0.
 */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL && func != NULL)
+	return;
 {
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
 }
-
